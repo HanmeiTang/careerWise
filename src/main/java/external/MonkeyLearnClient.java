135b467d29c56c4c466eedb2c39c23095c4fd450
Hanmei Tang <hmtang003@gmail.com>
2020-10-08 14:12:27 -0700

    discard yaml

--- src/main/java/external/MonkeyLearnClient.java
+++ src/main/java/external/MonkeyLearnClient.java
@@ -1,13 +1,41 @@
 package external;
 
 import java.io.File;
+import java.io.InputStream;
+import java.io.IOException;
+import java.util.Map;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.yaml.snakeyaml.Yaml;
 
 public class MonkeyLearnClient {
 	// Note: set MONKEY_LEARN_API_KEY in APP_ROOT_DIR/.careerwise.yaml
-	private static final String API_KEY = System.getenv("MONKEY_LEARN_API_KEY");
+	
+	private static String API_KEY;
+	
+	public MonkeyLearnClient() {
+		//API_KEY = 
+		readApiKey();
+	}
+	
+	private void readApiKey() {
+		Yaml yaml = new Yaml();
+		String dir = new File(System.getProperty("user.dir") + "/.careerwise.yaml").toString();
+		
+		InputStream istr = this.getClass()
+				.getClassLoader()
+				.getResourceAsStream(dir);
+		//System.out.print(file.toString());
+		Map<String, Object> dic = yaml.load(istr);
+		
+		
+		//return dic.get("MONKEY_LEARN_API_KEY"); 
+	}
 	public static void main(String[] args) {
-		System.out.print(System.getenv());
+		MonkeyLearnClient mkc = new MonkeyLearnClient();
+		
+		//System.out.println(new File(System.getProperty("user.dir") + "/.careerwise.yaml"));
+		
+
 	}
 }
