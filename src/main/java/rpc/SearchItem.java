daec03fb2d0d881a02d786e942efbc6a959ff729
Hanmei Tang <hmtang003@gmail.com>
2020-10-08 09:23:45 -0700

    add search result to servlet

--- src/main/java/rpc/SearchItem.java
+++ src/main/java/rpc/SearchItem.java
@@ -9,45 +9,55 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import external.GitHubClient;
 
 /**
  * Servlet implementation class SearchItem
  */
 public class SearchItem extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public SearchItem() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// Get user name from HTTP query
-		JSONArray array = new JSONArray();
-		array.put(new JSONObject().put("username", "abcd"));
-		array.put(new JSONObject().put("username", "1234"));
-		
-		RpcHelper.writeJsonArray(response, array);
-		
-//		if (request.getParameter("username") != null) {
-//			JSONObject obj = new JSONObject();
-//			String username = request.getParameter("username");
-//			obj.put("username", username);
-//			writer.print(obj);
-//		}
-		
+	public SearchItem() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// Get user name from HTTP query
+		double lat = Double.parseDouble(request.getParameter("lat"));
+		double lon = Double.parseDouble(request.getParameter("lon"));
+
+		// test
+		// JSONArray array = new JSONArray();
+		// array.put(new JSONObject().put("username", "abcd"));
+		// array.put(new JSONObject().put("username", "1234"));
+
+		GitHubClient client = new GitHubClient();
+		RpcHelper.writeJsonArray(response, client.search(lat, lon, null));
+
+		// if (request.getParameter("username") != null) {
+		// JSONObject obj = new JSONObject();
+		// String username = request.getParameter("username");
+		// obj.put("username", username);
+		// writer.print(obj);
+		// }
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
