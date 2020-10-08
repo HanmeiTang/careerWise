2cbdd233401c73b6f87b7464fa395d382c0d726e
Hanmei Tang <hmtang003@gmail.com>
2020-10-08 09:18:45 -0700

    add github client

--- src/main/java/external/GitHubClient.java
+++ src/main/java/external/GitHubClient.java
@@ -17,4 +17,47 @@ import org.json.JSONArray;
 public class GitHubClient {
 	private static final String URL_TEMPLATE = "https://jobs.github.com/positions.json?description=%s&lat=%s&long=%s";
 	private static final String DEFAULT_KEYWORD = "developer";
+
+	public JSONArray search(double lat, double lon, String keyword) {
+		if (keyword == null) {
+			keyword = DEFAULT_KEYWORD;
+		}
+		try {
+			keyword = URLEncoder.encode(keyword, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+
+		String url = String.format(URL_TEMPLATE, keyword, lat, lon);
+
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+
+		// Create a custom response handler
+		ResponseHandler<JSONArray> responseHandler = new ResponseHandler<JSONArray>() {
+
+			@Override
+			public JSONArray handleResponse(final HttpResponse response) throws IOException {
+				if (response.getStatusLine().getStatusCode() != 200) {
+					return new JSONArray();
+				}
+				HttpEntity entity = response.getEntity();
+				if (entity == null) {
+					return new JSONArray();
+				}
+				String responseBody = EntityUtils.toString(entity);
+				return new JSONArray(responseBody);
+			}
+		};
+
+		try {
+			return httpclient.execute(new HttpGet(url), responseHandler);
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return new JSONArray();
+	}
+
 }
