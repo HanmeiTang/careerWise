97c22a7e28205bf4244243b7d571f02a1984e255
Hanmei Tang <hmtang003@gmail.com>
2020-10-08 17:18:36 -0700

    enable yaml config

--- src/main/java/db/MySQLDBUtil.java
+++ src/main/java/db/MySQLDBUtil.java
@@ -1,14 +1,13 @@
 package db;
 
+import util.YamlUtil;
+
 public class MySQLDBUtil {
-	// TODO: Add this to config file
-	private static final String INSTANCE = "careerwise-db.citiv7qnn1e7.us-east-2.rds.amazonaws.com";
+	private static final String INSTANCE = YamlUtil.getContent().get("MODEL_ID");
 	private static final String PORT_NUM = "3306";
-	// TODO: Add this to config file
-	public static final String DB_NAME = "careerWise";
-	private static final String USERNAME = "admin";
-	// TODO: Add this to config file
-	private static final String PASSWORD = "YOUR_PASSWORD";
+	public static final String DB_NAME = YamlUtil.getContent().get("DB_NAME");
+	private static final String USERNAME = YamlUtil.getContent().get("USERNAME");
+	private static final String PASSWORD = YamlUtil.getContent().get("PASSWORD");
 	public static final String URL = "jdbc:mysql://" + INSTANCE + ":" + PORT_NUM + "/" + DB_NAME + "?user=" + USERNAME
 			+ "&password=" + PASSWORD + "&autoReconnect=true&serverTimezone=UTC";
 
---
+++ src/main/java/util/YamlUtil.java
@@ -0,0 +1,39 @@
+package util;
+
+import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.HashMap;
+import org.yaml.snakeyaml.Yaml;
+
+
+public class YamlUtil {
+	public static String currentDir = System.getProperty("user.dir");
+	public static String yamlName = ".careerwise.yaml";
+	public String yaml_dir;
+	
+	public static HashMap<String, String> getContent() {
+		Yaml yaml = new Yaml();
+		String input = currentDir + "/" + yamlName;
+		
+		try {
+			InputStream inputStream = new FileInputStream(input);
+			HashMap<String, String> map = yaml.loadAs(inputStream, HashMap.class);
+			return map;
+		} catch (IOException e) {
+			String format = "Error read %s\n";
+			System.out.printf(format, input);
+		}
+		
+		
+		return null;
+		
+	}
+	
+	public static void main(String[] args) {
+		// Quick demo
+		System.out.println(YamlUtil.currentDir);
+		System.out.println(YamlUtil.getContent().get("MODEL_ID"));
+	}
+
+}
